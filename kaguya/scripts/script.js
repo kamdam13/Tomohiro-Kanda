eafb556126575ab27d4caaff9fc96b1da6de4798
kandam <h1ro.kmd13@gmail.com>
2020-10-29 00:25:52 +0900

    ハンバーガーメニュー実装

--- kaguya/scripts/script.js
+++ kaguya/scripts/script.js
@@ -1,10 +1,15 @@
 'use strict';
 
-$('a:not([href^="#"]):not([target])').click((e) => {
+$('a:not([href^="#"]):not([target])').on('click',(e) => {
   e.preventDefault();
   $('body')
     .on('transitionend', () => {
-      window.location = e.target.href;
+      window.location = e.currentTarget.href;
     })
     .css('opacity', 0.1);
+});
+
+$('#header-nav-button').on('click', () => {
+  $('.header-nav').slideToggle();
+  $('.nav-button-line').toggleClass('active');
 });
